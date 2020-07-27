f20c30f8329ca1093a1e9a9d0e0dbeb3b9317f1a
fedeziraldo <federrico04@gmail.com>
2020-07-27 19:59:01 -0300

    commit inicial chat

--- bin/mongodb.js
+++ bin/mongodb.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect(`mongodb://${process.env.MONGO_DB_HOST}/${process.env.MONGO_DB_DB}`, { useNewUrlParser: true }, function(error){
+mongoose.connect(`mongodb://${process.env.MONGO_DB_HOST}/${process.env.MONGO_DB_DB}`, { useNewUrlParser: true, useUnifiedTopology: true}, function(error){
    if(error){
       throw error; 
    }else{
---
+++ socket.js
@@ -0,0 +1,19 @@
+const socketIo = require("socket.io");
+let io;
+
+const iniciar = (server) => {
+    io = socketIo(server);
+
+    io.on("connection", (socket) => {
+        console.log("New client connected");
+        socket.on("disconnect", () => {
+            console.log("Client disconnected");
+        });
+        socket.on("chat", msj => {
+            console.log(msj);
+            io.emit("chat", msj)
+        });
+    });
+}
+
+module.exports = iniciar
