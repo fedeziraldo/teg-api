a6fa7a8d6de93f9ec238b307ec09678a1363db14
fedeziraldo <federrico04@gmail.com>
2020-09-17 20:02:57 -0300

    login duradero

--- bin/socket.js
+++ bin/socket.js
@@ -6,10 +6,37 @@ const Sala = require('../Sala/Sala')
 const usuario = require("../model/usuario")
 let io
 
+/**
+ * lista de Sala
+ */
 let salas = []
+/**
+ * lista de Conexion
+ */
 let conexiones = []
 
 const mensajeUnidoASala = `Te has unido a la sala `
+
+/**
+ * en caso de una salida filtrar las salas y los integrantes 
+ * @param {*} conexion 
+ */
+const filtrarSalasALSalir = conexion => {
+    const sala = salas.find(sala => sala.userId == conexion.userId)
+    if (sala) {
+        for (let integrante in sala.integrantes) {
+            const conIntegrante = conexiones.find(con => con.userId == integrante)
+            if (conIntegrante) {
+                conIntegrante.socket.leave(sala.userId)
+            }
+        }
+    }
+    for (let sala of salas) {
+        sala.integrantes = sala.integrantes.filter(integrante => integrante != conexion.userId)
+    }
+    salas = salas.filter(sala => sala.userId != conexion.userId)
+}
+
 
 const iniciar = (server) => {
     io = socketIo(server);
@@ -20,20 +47,8 @@ const iniciar = (server) => {
             const conexion = conexiones.find(con => con.socket == socket)
             if (conexion) {
                 console.log("Client disconnected", conexion.userId);
+                filtrarSalasALSalir(conexion)
                 conexiones = conexiones.filter(con => con.socket != socket)
-                for (let sala of salas) {
-                    sala.integrantes = sala.integrantes.filter(integrante => integrante != conexion.userId)
-                }
-                const sala = salas.find(sala => sala.userId == conexion.userId)
-                if (sala) {
-                    for (let integrante in sala.integrantes) {
-                        const conIntegrante = conexiones.find(con => con.userId == integrante)
-                        if (conIntegrante) {
-                            conIntegrante.socket.leave(conexion.userId)
-                        }
-                    }
-                }
-                salas = salas.filter(sala => sala.userId != conexion.userId)
                 io.emit("salas", salas)
             }
         });
@@ -45,13 +60,34 @@ const iniciar = (server) => {
                 } else {
                     console.log("New client connected", decoded.id);
                     const usu = await usuario.findById(decoded.id)
-                    conexiones.push(new Conexion(token, socket, decoded.id, usu))
-                    socket.emit("usuario", usu)
-                    socket.emit("salas", salas)
+                    const conexion = conexiones.find(con => con.userId == decoded.id)
+                    if (conexion) {
+                        const sala = salas.find(sala => sala.userId == conexion.socket.sala)
+                        if (sala) {
+                            conexion.socket.leave(conexion.socket.sala)
+                            socket.join(sala.userId)
+                            socket.sala = sala.userId
+                            socket.emit("chat", `${mensajeUnidoASala} ${sala.userId}`)
+                            socket.emit("sala", sala)
+                            socket.emit("usuario", usu)
+                            socket.emit("salas", salas)
+                            conexion.token = token
+                            conexion.socket = socket
+                            return
+                        } else {
+                            conexion.socket.leave("sin sala")
+                            conexion.token = token
+                            conexion.socket = socket
+                        }
+                    } else {
+                        conexiones.push(new Conexion(token, socket, decoded.id, usu))
+                    }
                     socket.join("sin sala")
                     socket.sala = "sin sala"
                     socket.emit("chat", `${mensajeUnidoASala} Sin sala`)
                     socket.emit("sala", 'sin sala')
+                    socket.emit("usuario", usu)
+                    socket.emit("salas", salas)
                 }
             })
         })
@@ -59,28 +95,19 @@ const iniciar = (server) => {
             const conexion = conexiones.find(con => con.socket == socket)
             if (conexion) {
                 console.log("Client deslogueado", conexion.userId);
-                conexiones = conexiones.filter(con => con.socket != socket)
-                for (let sala of salas) {
-                    sala.integrantes = sala.integrantes.filter(integrante => integrante != conexion.userId)
-                }
-                const sala = salas.find(sala => sala.userId == conexion.userId)
-                if (sala) {
-                    for (let integrante in sala.integrantes) {
-                        const conIntegrante = conexiones.find(con => con.userId == integrante)
-                        if (conIntegrante) {
-                            conIntegrante.socket.leave(conexion.userId)
-                        }
-                    }
-                }
+                filtrarSalasALSalir(conexion)
                 socket.leave("sin sala")
-                salas = salas.filter(sala => sala.userId != conexion.userId)
+                conexiones = conexiones.filter(con => con.socket != socket)
                 socket.sala = ""
                 io.emit("salas", salas)
             }
         })
         socket.on("chat", msj => {
-            console.log(msj);
-            io.to(socket.sala).emit("chat", msj)
+            const conexion = conexiones.find(con => con.socket == socket)
+            if (conexion) {
+                console.log(msj);
+                io.to(socket.sala).emit("chat", msj)
+            }
         });
         socket.on("crearSala", async () => {
             const conexion = conexiones.find(con => con.socket == socket)
@@ -89,9 +116,9 @@ const iniciar = (server) => {
                 const sala = new Sala(conexion.userId, usu)
                 salas.push(sala)
                 socket.leave("sin sala")
-                socket.join(conexion.userId)
-                socket.sala = conexion.userId
-                socket.emit("chat", `${mensajeUnidoASala} ${conexion.userId}`)
+                socket.join(sala.userId)
+                socket.sala = sala.userId
+                socket.emit("chat", `${mensajeUnidoASala} ${sala.userId}`)
                 socket.emit("sala", sala)
                 io.emit("salas", salas)
             }
@@ -106,7 +133,7 @@ const iniciar = (server) => {
                     socket.join(sala.userId)
                     socket.sala = sala.userId
                     socket.emit("chat", `${mensajeUnidoASala} ${conexion.userId}`)
-                    socket.emit("sala", sala)
+                    io.to(sala.userId).emit("sala", sala)
                     io.emit("salas", salas)
                 }
             }
@@ -114,19 +141,8 @@ const iniciar = (server) => {
         socket.on("abandonarSala", () => {
             const conexion = conexiones.find(con => con.socket == socket)
             if (conexion) {
-                for (let sala of salas) {
-                    sala.integrantes = sala.integrantes.filter(integrante => integrante != conexion.userId)
-                }
-                const sala = salas.find(sala => sala.userId == conexion.userId)
-                if (sala) {
-                    for (let integrante in sala.integrantes) {
-                        const conIntegrante = conexiones.find(con => con.userId == integrante)
-                        if (conIntegrante) {
-                            conIntegrante.socket.leave(conexion.userId)
-                        }
-                    }
-                }
-                salas = salas.filter(sala => sala.userId != conexion.userId)
+                filtrarSalasALSalir(conexion)
+                socket.leave(socket.sala)
                 socket.join("sin sala")
                 socket.sala = "sin sala"
                 socket.emit("chat", `${mensajeUnidoASala} Sin sala`)
